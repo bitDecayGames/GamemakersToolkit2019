7aecf6445a3405ed651c2b903292c706d89af4e7
unknown <zombiefish90@gmail.com>
2019-08-03 11:23:26 -0600

    Instantiate prefabs based on textfiles

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -8,6 +8,7 @@ public class Scenes
 	public const string GameJamSplash = "GameJamSplash";
 	public const string Test = "Test";
 	public const string TestKabobSpear = "TestKabobSpear";
+	public const string FisherTest = "FisherTest";
 	public enum SceneEnum
 	{
 		TitleScreen = 98,
@@ -16,6 +17,7 @@ public class Scenes
 		GameJamSplash = 253,
 		Test = 160,
 		TestKabobSpear = 122,
+		FisherTest = 1,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
@@ -33,6 +35,8 @@ public class Scenes
 				return Test;
 			case SceneEnum.TestKabobSpear:
 				return TestKabobSpear;
+			case SceneEnum.FisherTest:
+				return FisherTest;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
