fe4df0ebab53e8fad1770ea78d129a00ca246a15
mwingfield <mwingfie@gmail.com>
2019-08-03 12:37:09 -0600

    got some animation helper classes

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -8,7 +8,6 @@ public class Scenes
 	public const string GameJamSplash = "GameJamSplash";
 	public const string Test = "Test";
 	public const string TestKabobSpear = "TestKabobSpear";
-	public const string FisherTest = "FisherTest";
 	public enum SceneEnum
 	{
 		TitleScreen = 98,
@@ -17,7 +16,6 @@ public class Scenes
 		GameJamSplash = 253,
 		Test = 160,
 		TestKabobSpear = 122,
-		FisherTest = 1,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
@@ -35,8 +33,6 @@ public class Scenes
 				return Test;
 			case SceneEnum.TestKabobSpear:
 				return TestKabobSpear;
-			case SceneEnum.FisherTest:
-				return FisherTest;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
