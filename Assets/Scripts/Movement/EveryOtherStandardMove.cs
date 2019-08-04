d41404db2977946cbb55b558e554ba430d8bf599
mwingfield <mwingfie@gmail.com>
2019-08-04 09:41:44 -0600

    added mushroom and throw animations

--- Assets/Scripts/Movement/EveryOtherStandardMove.cs
+++ Assets/Scripts/Movement/EveryOtherStandardMove.cs
@@ -1,20 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class EveryOtherStandardMove: MonoBehaviour, IMovementBehavior
-{
+public class EveryOtherStandardMove : MonoBehaviour, IMovementBehavior {
     bool doMove = false;
 
-    public MovementIntent GetMovementIntent(Vector2 direction)
-    {
-        if(doMove)
-        {
+    public Sprite awakeSprite;
+    public Sprite sleepingSprite;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start() {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public MovementIntent GetMovementIntent(Vector2 direction) {
+        if (doMove) {
             doMove = false;
+            SetSprite();
             return new MovementIntent(direction);
         } else {
             doMove = true;
+            SetSprite();
             return new MovementIntent(Directions.None);
+        }
+    }
+
+    private void SetSprite() {
+        if (doMove) {
+            _spriteRenderer.sprite = awakeSprite;
+        } else {
+            _spriteRenderer.sprite = sleepingSprite;
         }
     }
 }
