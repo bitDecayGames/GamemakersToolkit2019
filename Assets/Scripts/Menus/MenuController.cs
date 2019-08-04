f24579506cb16137f960a53d8c804c78d2e1f158
Tanner <Shadoninja@gmail.com>
2019-08-04 10:43:04 -0600

    ye

--- Assets/Scripts/Menus/MenuController.cs
+++ Assets/Scripts/Menus/MenuController.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using Utils;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
+
+    private bool _startPressed;
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !_startPressed) {
             FMODMusicPlayer.Instance.StopThenDestroy();
             CurrentLevelNumber.Instance.LevelNumber = CurrentLevelNumber.Instance.HighestLevel; // first level or highest level reached
             GetComponent<EasyNavigator>().GoToScene("Tutorial01");
             FMODSoundEffectsPlayer.Instance.PlaySoundEffect(SFX.PressStart);
+
+            _startPressed = true;
         }
     }
 }
